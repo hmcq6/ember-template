6f802de4945b418c4faf7bb294574264b99e2b1e
Hassan McKusick <Hassan.McKusick@gmail.com>
2018-01-15 18:46:38 -0500

    Log in working

---
+++ app/components/login-form.js
@@ -0,0 +1,15 @@
+import Component from '@ember/component';
+import { inject } from '@ember/service';
+
+export default Component.extend({
+  session: inject(),
+
+  actions: {
+    authenticate() {
+      const { identification, password } = this.getProperties('identification', 'password');
+      return this.get('session').authenticate('authenticator:devise', identification, password).catch(({ error }) => {
+        this.set('errorMessage', error);
+      });
+    }
+  }
+});
