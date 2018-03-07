712d747d1d2c2b73210dbbb32688d61e558deb8d
Hassan McKusick <Hassan.McKusick@gmail.com>
2018-03-07 00:23:19 -0500

    Create sale on paypal sandbox

--- app/services/payment.js
+++ app/services/payment.js
@@ -9,7 +9,7 @@ export default Service.extend({
       headers: {
         'Accept': 'application/json',
         'Accept-Language': 'en-US',
-        'Authorization': `Basic ${ btoa('') }`
+        'Authorization': `Basic ${ btoa('AXYUgv3rUTOcfhQC_BNVvR73k7r9aMitG0zsB7kJ7IIwvTy7ZOwb8h44EZ_ANQYQrAT8dNofXMbKrDP5:EC2gxAQnxzIiLj74tC8AMl-mPoWFNj5Ep8-fc12tqgSSMoFBxFYMopNPkZdCBp7j5BO9J48Gz0Y7lqOX') }`
       },
       method: 'POST',
       data: 'grant_type=client_credentials'
@@ -24,6 +24,8 @@ export default Service.extend({
           accessToken = this.get('accessToken');
     return this.get('ajax').request('https://api.sandbox.paypal.com/v1/payments/payment', {
       headers: {
+        'Content-Type': 'application/json',
+        'Accept-Language': 'en-US',
         'Authorization': `Bearer ${ accessToken }`
       },
       method: 'POST',
@@ -32,43 +34,65 @@ export default Service.extend({
   },
   _generateTransactionTemplate() {
     return {
-      intent: 'sale',
-      payer: {
-        payment_method: 'credit_card',
-        funding_instruments: [{
-          credit_card: {
-            type: 'visa',
-            number: '4111111111111111',
-            expire_month: 2,
-            expire_year: 2018,
-            cvv2: '123',
-            first_name: 'test',
-            last_name: 'test',
-            billing_address: {
-              line1: '123 street',
-              city: 'CityVille',
-              state: 'CA',
-              postal_code: '12345',
-              country_code: 'US'
-            }
-          }
-        }]
+      "intent": "sale",
+      "payer": {
+        "payment_method": "paypal"
       },
-      transactions: [{
-        amount: {
-          currency: 'USD',
-          total: '30',
-          details: {
-            subtotal: '30'
+      "transactions": [{
+        "amount": {
+          "total": "30.11",
+          "currency": "USD",
+          "details": {
+            "subtotal": "30.00",
+            "tax": "0.07",
+            "shipping": "0.03",
+            "handling_fee": "1.00",
+            "shipping_discount": "-1.00",
+            "insurance": "0.01"
           }
         },
-        description: 'FE test'
+        "description": "The payment transaction description.",
+        "custom": "EBAY_EMS_90048630024435",
+        "invoice_number": "48787589673",
+        "payment_options": {
+          "allowed_payment_method": "INSTANT_FUNDING_SOURCE"
+        },
+        "soft_descriptor": "ECHI5786786",
+        "item_list": {
+          "items": [{
+            "name": "hat",
+            "description": "Brown hat.",
+            "quantity": "5",
+            "price": "3",
+            "tax": "0.01",
+            "sku": "1",
+            "currency": "USD"
+          }, {
+            "name": "handbag",
+            "description": "Black handbag.",
+            "quantity": "1",
+            "price": "15",
+            "tax": "0.02",
+            "sku": "product34",
+            "currency": "USD"
+          }],
+          "shipping_address": {
+            "recipient_name": "Brian Robinson",
+            "line1": "4th Floor",
+            "line2": "Unit #34",
+            "city": "San Jose",
+            "country_code": "US",
+            "postal_code": "95131",
+            "phone": "011862212345678",
+            "state": "CA"
+          }
+        }
       }],
-      redirect_urls: {
-        return_url: 'http://localhost:4200/checkout',
-        cancel_url: 'http://localhost:4200/checkout'
+      "note_to_payer": "Contact us for any questions on your order.",
+      "redirect_urls": {
+        "return_url": "https://example.com/return",
+        "cancel_url": "https://example.com/cancel"
       }
     };
   }
-
 });
